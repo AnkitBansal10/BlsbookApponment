b2d53d42c8b982fe22020f28f31ce56fb03e4d9b
AnkitBansal10 <ankit.bansal@blsinternational.com>
2025-06-27 15:44:01 +0530

    Update authApi.js

---
+++ src/api/authApi.js
@@ -0,0 +1,2 @@
+// "https://development.blsinternational.com/Italy_pakistan_appmnt/api/example/applicant_login"
+// "https://development.blsinternational.com/Italy_pakistan_appmnt/api/example/applicant_registration"
